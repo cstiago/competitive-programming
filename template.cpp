8f27fa3efb0bab401bf0f81cc60fcb7c1f637eec
Tiago Cunha <tiagocunha.me@gmail.com>
2025-10-15 17:26:08 -0300

    feat(template): add defines and set fast i/o and test cases loop to single lines

--- template.cpp
+++ template.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 typedef long long ll;
 
-void solve() {
+#define loop(i_,n_) for(ll i_=0;i_<n_;i_++)
+#define each(i_,v_) for(auto& i_:v_)
+#define endl '\n'
 
+void solve() {
+    
 }
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); cout.tie(0);
-  
-  ll t; cin >> t; while(t--)
-  solve();
-
-  return 0;
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    
+    int t; cin >> t; while(t--)
+        solve();
 }
