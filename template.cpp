5c4c5dcd083f499d06be74758d602a35d8f04d8e
Tiago Cunha <tiagocunha.me@gmail.com>
2025-11-21 11:08:57 -0300

    feat(template): add file io

--- template.cpp
+++ template.cpp
@@ -1,17 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef __int128_t lll;
 
 #define loop(i_,n_) for(ll i_=0;i_<n_;i_++)
 #define each(i_,v_) for(auto& i_:v_)
 
+void fastio() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+}
+
+void fileio(string s) {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
 void solve() {
-   
+    
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    fastio();
+    // fileio("");
     
     int t; cin >> t; while(t--)
-        solve();
+    solve();
 }
