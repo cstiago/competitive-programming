951f8e528deaffca1f993b2a29f94b55ca5fc9a4
Tiago Cunha <tiagocunha.me@gmail.com>
2025-11-21 11:15:44 -0300

    refactor(template): set solve function up

--- template.cpp
+++ template.cpp
@@ -6,6 +6,10 @@ typedef __int128_t lll;
 #define loop(i_,n_) for(ll i_=0;i_<n_;i_++)
 #define each(i_,v_) for(auto& i_:v_)
 
+void solve() {
+    
+}
+
 void fastio() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 }
@@ -13,10 +17,6 @@ void fastio() {
 void fileio(string s) {
     freopen((s + ".in").c_str(), "r", stdin);
     freopen((s + ".out").c_str(), "w", stdout);
-}
-
-void solve() {
-    
 }
 
 int main() {
