bd8738113e64227ffe7965e29456b2cabbb8d61f
Tiago Cunha <tiagocunha.me@gmail.com>
2025-11-26 10:42:34 -0300

    refactor(template): shorten io

--- template.cpp
+++ template.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using lll = __int128;
 
 void solve() {
     
 }
 
 void io(const string& s = "") {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    if(!s.empty()) {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    if(s.size()) {
         freopen((s + ".in").c_str(), "r", stdin);
         freopen((s + ".out").c_str(), "w", stdout);
     }
