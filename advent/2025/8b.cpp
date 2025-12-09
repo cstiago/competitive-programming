97214a8ddb7cf91ac497f3c97219d2fe8de86619
Tiago Cunha <tiagocunha.me@gmail.com>
2025-12-09 02:16:39 -0300

    feat(advent-2025): add day 8

---
+++ advent/2025/8b.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+#define dbg(x) cerr << (#x) << " is " << (x) << "\n"
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+const ll mod = 1e9+7;
+
+struct box {
+    ll x, y, z;
+    int dad, qt = 1;
+    box(ll x_, ll y_, ll z_, int dad_){
+        x = x_; y = y_; z = z_;
+        dad = dad_;
+    };
+};
+
+vector<box> v;
+
+ll p(ll n) {
+    return n*n;
+}
+
+ll dist(pair<int,int> bs) {
+    return p(v[bs.first].x-v[bs.second].x) + p(v[bs.first].y-v[bs.second].y) + p(v[bs.first].z-v[bs.second].z);
+}
+
+int find(int i) {
+    if(v[i].dad == i) return i;
+
+    return v[i].dad = find(v[i].dad);
+}
+
+void unite(int ri, int rj) {
+    v[rj].dad = ri;
+    v[ri].qt += v[rj].qt;
+}
+
+void solve() {
+    string s; stringstream ss;
+    ll x, y, z;
+    char _;
+    while(getline(cin, s)) {
+        ss.clear(); ss.str(s);
+        ss >> x >> _ >> y >> _ >> z;
+
+        box b(x, y, z, v.size());
+        v.push_back(b);
+    }
+    
+    auto cmp = [](pair<int,int> p1, pair<int,int> p2) {
+        return dist(p1) > dist(p2);
+    };
+
+    priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)> mindist(cmp);
+
+    for(int i = 0; i < v.size(); i++) {
+        for(int j = i+1; j < v.size(); j++) {
+            mindist.push({i,j});
+        }
+    }
+
+    ll ans;
+    while(!mindist.empty()) {
+        auto b = mindist.top();
+        mindist.pop();
+
+        int i = find(b.first), j = find(b.second);
+
+        if(i != j) {
+            unite(i, j);
+            ans = v[b.first].x * v[b.second].x;
+        }
+    }
+
+    cout << ans << "\n";
+}
+
+void io(const string& s = "") {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    if(s.size()) {
+        freopen((s + ".in").c_str(), "r", stdin);
+        freopen((s + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    io("8b");
+    ull t = 1;
+    while(t--) {
+        solve();
+    }
+}
