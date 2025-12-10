c2ddb218adcb2d07db71346b7dcc92197f813b9b
Tiago Cunha <tiagocunha.me@gmail.com>
2025-12-09 23:52:31 -0300

    feat(template): add debugging definition and constants

--- template.cpp
+++ template.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 
+#define dbg(x) cerr << (#x) << " is " << (x) << "\n"
+
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 using str = string;
+
+const int mn = INT_MIN;
+const int mx = INT_MAX;
+const ll lmn = LLONG_MIN;
+const ll lmx = LLONG_MAX;
+const ll mod = 1e9+7;
 
 void solve() {
 
@@ -20,10 +28,8 @@ void io(const string& s = "") {
 
 int main() {
     io("");
-    
     ull t = 1;
     cin >> t;
-
     while(t--) {
         solve();
     }
